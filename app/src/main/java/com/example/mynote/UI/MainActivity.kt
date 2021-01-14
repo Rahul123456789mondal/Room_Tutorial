3281aeddabfab09fc2fc8a9be39f81f9f2aceb1c
Arka Mondal <arkamondal1995@outlook.com>
2021-01-14 21:51:36 +0530

    To Add the Fragment and navigation Fragment

--- app/src/main/java/com/example/mynote/UI/MainActivity.kt
+++ app/src/main/java/com/example/mynote/UI/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mynote
+package com.example.mynote.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mynote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
